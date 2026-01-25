2f1dfbc061268d51d58306dd288f9c15e0117521
Yashwant Namdev <64881194+CodeCrafterYashwant@users.noreply.github.com>
2026-01-25 15:10:34 +0530

    Update jwt.js

--- jwt.js
+++ jwt.js
@@ -16,6 +16,6 @@ const jwtAuthMiddleware = (req,res,next) =>{
 }
 
 const generateToken = (userData) =>{
-    return jwt.sign(userData,process.env.JWT_SECRET,{expiresIn: 50});
+    return jwt.sign(userData,process.env.JWT_SECRET,{expiresIn: '1d'});
 }
 module.exports = {jwtAuthMiddleware,generateToken};
